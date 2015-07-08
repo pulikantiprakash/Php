41fba664f19ec07ff9ea54bc7dcbf3d6368c3fb1
pulikantiprakash <pulikantiprakash17@gmail.com>
2015-07-08 17:36:29 +0530

    directory_copy_helper.php

---
+++ directory_copy_helper.php
@@ -0,0 +1,63 @@
+<?php  
+defined('BASEPATH') OR exit('No direct script access allowed');    
+
+if ( ! function_exists('directory_map'))
+{
+	function directory_map($source_dir, $directory_depth = 0, $hidden = FALSE)
+	{
+		if ($fp = @opendir($source_dir))
+		{
+			$filedata	= array();
+			$new_depth	= $directory_depth - 1;
+			$source_dir	= rtrim($source_dir, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
+
+			while (FALSE !== ($file = readdir($fp)))
+			{
+				// Remove '.', '..', and hidden files [optional]
+				if ( ! trim($file, '.') OR ($hidden == FALSE && $file[0] == '.'))
+				{
+					continue;
+				}
+
+				if (($directory_depth < 1 OR $new_depth > 0) && @is_dir($source_dir.$file))
+				{
+					$filedata[$file] = directory_map($source_dir.$file.DIRECTORY_SEPARATOR, $new_depth, $hidden);
+				}
+				else
+				{
+					$filedata[] = $file;
+				}
+			}
+
+			closedir($fp);
+			return $filedata;
+		}
+
+		return FALSE;
+	}
+}
+ 
+if(!function_exists('directory_copy'))
+{
+    function directory_copy($srcdir, $dstdir)
+    {
+        //preparing the paths
+        $srcdir=rtrim($srcdir,'/');
+        $dstdir=rtrim($dstdir,'/');
+
+        //creating the destination directory
+        if(!is_dir($dstdir))mkdir($dstdir, 0777, true);
+
+        //Mapping the directory
+        $dir_map=directory_map($srcdir);
+
+        foreach($dir_map as $object_key=>$object_value)
+        {
+            if(is_numeric($object_key))
+                copy($srcdir.'/'.$object_value,$dstdir.'/'.$object_value);//This is a File not a directory
+            else
+                directory_copy($srcdir.'/'.$object_key,$dstdir.'/'.$object_key);//this is a directory
+        }
+    }
+}
+ 
